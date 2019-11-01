e725cf48eb6dbd29a936686aafa8b303a14c5aeb
Phuong Ubuntu18 <duyphuonqsc@gmail.com>
2019-11-01 13:38:19 +0700

    Add start scence

--- TodoApp/MyApp.gd
+++ TodoApp/MyApp.gd
@@ -3,11 +3,18 @@ onready var Bar = preload("res://bar.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var b = Bar.instance()
-	#b.get_node("bar").move_and_slide(velocity)
-	$Container/ScrollView.add_child(b)
+	$HTTPRequest.request("https://raw.githubusercontent.com/duyphuonq/todoapp_godot/master/data.json")
 
 func _spawn():
 	var b = Bar.instance()
-	#b.get_node("bar").move_and_slide(velocity)
+	$Container/ScrollView.add_child(b)
+
+func _on_HTTPGet_request_completed(result, response_code, headers, body):
+	var json = JSON.parse(body.get_string_from_utf8())
+	for x in json.result["note"]:
+		var b = Bar.instance()
+		b.get_node("Bar/LineEdit").set_text(x)
+		b.get_node("Bar/CheckBox")
+		$Container/ScrollView.add_child(b)
+	var b = Bar.instance()
 	$Container/ScrollView.add_child(b)
--- TodoApp/bar.gd
+++ TodoApp/bar.gd
@@ -1,21 +1,21 @@
 extends Control
 var can_spwan = true
+#var syncc = false
 
 func _ready():
-	$Bar/CheckBox.hide()
-	
+	if not get_node("Bar/LineEdit").get_text():
+		$Bar/CheckBox.hide()
 func _on_LineEdit_text_entered(new_text):
-	var children = get_tree().get_root().get_node("MyApp/Container/ScrollView").get_child_count()
-	if (can_spwan and new_text) or children == 1:
+	if (can_spwan and new_text):
 		var View = get_tree().get_root().get_node("MyApp")._spawn()
-		can_spwan = false	
+		can_spwan = false
 func _on_CheckBox_pressed():
 	var children = get_tree().get_root().get_node("MyApp/Container/ScrollView").get_child_count()
 	if children > 1:
 		get_node(".").queue_free()
 	
 func _on_LineEdit_text_changed(new_text):
-	if new_text:
+	if new_text or get_node("Bar/LineEdit").get_text():
 		$Bar/CheckBox.show()
 	else:
 		$Bar/CheckBox.hide()
